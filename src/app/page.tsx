195bcc3a0ddc353d6c00556121cb5e863a51f3b9
davidGBbr <davidgomesbrigido@gmail.com>
2023-11-02 11:52:54 -0300

    Social Links created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@
 import React from "react";
 import NavBar from "@/components/NavBar";
 import Home from "@/components/Home";
+import SocialLinks from "@/components/SocialLinks";
 
 const Page = () => {
   return (
     <div>
       <NavBar />
       <Home />
+      <SocialLinks />
     </div>
   );
 };
---
+++ src/components/SocialLinks.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+
+const SocialLinks = () => {
+  const links = [
+    {
+      id: 1,
+      child: (
+        <>
+          Linkedin <FaLinkedin size={30} />
+        </>
+      ),
+      href: "https://www.linkedin.com/in/david-brigido/",
+      style: "rounded-tr-md",
+      target: true,
+    },
+    {
+      id: 2,
+      child: (
+        <>
+          GitHub <FaGithub size={30} />
+        </>
+      ),
+      href: "https://github.com/DavidGBbr",
+      target: true,
+    },
+    {
+      id: 3,
+      child: (
+        <>
+          Currículo <BsFillPersonLinesFill size={30} />
+        </>
+      ),
+      href: "https://drive.google.com/uc?export=download&id=1KNxhQ-K7B_i3C_oml2JVq5IbLWZU2F1g",
+      style: "rounded-br-md",
+      download: true,
+      type: "application/pdf",
+      target: false,
+    },
+  ];
+
+  return (
+    <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
+      <ul>
+        {links.map(({ id, child, href, style, download, type, target }) => (
+          <li
+            key={id}
+            className={`flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500 ${style}`}
+          >
+            <a
+              href={href}
+              download={download}
+              target={target ? "_blank" : "_self"}
+              className="flex justify-between items-center w-full text-white"
+              type={type}
+            >
+              {child}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SocialLinks;
